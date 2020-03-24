00f8a4fbcfd3149d1cb9d9da4b6adf5a677daef7
Zahra Ali Aghazadeh <Zahra@Zahras-MacBook-Pro.local>
2020-03-24 02:14:22 -0700

    html design

--- script.js
+++ script.js
@@ -1,2 +1,39 @@
-// script
-console.log("here"); 
+
+// jQuery for the main carousel
+$(document).ready(function(){
+  $('.carousel').carousel();
+  fullWidth: true
+
+});
+
+
+// to give carousel a time interval but unfortunately it also gave time to the other pictures! :/
+// setInterval(function(){
+//   $('.carousel').carousel('next');
+// }, 3000);
+
+
+    
+
+// start carrousel
+$('.carousel.carousel-slider').carousel({
+  fullWidth: true,
+  indicators: false
+});
+
+
+// move next carousel
+$('.moveNextCarousel').click(function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  $('.carousel').carousel('next');
+});
+
+// move prev carousel
+$('.movePrevCarousel').click(function(e){
+  e.preventDefault();
+  e.stopPropagation();
+  $('.carousel').carousel('prev');
+});
+
+
